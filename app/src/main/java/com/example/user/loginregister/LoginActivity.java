8e48dd264750d777b0b4857a08cd954827284fde
p1415604x <p1415604x>
2017-10-08 16:45:23 +0100

    Initial commint

---
+++ app/src/main/java/com/example/user/loginregister/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.user.loginregister;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterhere);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+bLogin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        final String username = etUsername.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        Response.Listener<String> responselistener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonresponse = new JSONObject(response);
+                    boolean success = jsonresponse.getBoolean("success");
+
+                    if (success) {
+                        String name = jsonresponse.getString("name");
+                        int age = jsonresponse.getInt("age");
+
+                        Intent intent = new Intent(LoginActivity.this, UserAreaActivity.class);
+                        intent.putExtra("name", name);
+                        intent.putExtra("username", username);
+                        intent.putExtra("age", age);
+
+                        LoginActivity.this.startActivity(intent);
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                        builder.setMessage("Login Failed")
+                                .setNegativeButton("Retry", null)
+                                .create()
+                                .show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        LoginRequest loginrequest = new LoginRequest(username, password, responselistener);
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+        queue.add(loginrequest);
+
+    }
+});
+
+    }
+}
